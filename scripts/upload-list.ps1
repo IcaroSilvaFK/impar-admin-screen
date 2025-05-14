604563377331e9771b258bbad5d2ed088a597b53
whyy <icarovsilva1@gmail.com>
2025-05-14 15:40:42 -0300

    feat: change upload script

--- scripts/upload-list.ps1
+++ scripts/upload-list.ps1
@@ -1,10 +1,5 @@
-try {
 
-    Write-Host "Subindo a lista..."
-    Invoke-PnPSiteTemplate -Path "Admin.xml" -ErrorAction Stop
-    Write-Host "Fim" -ForegroundColor Green
+Write-Host "Subindo a lista..."
+Invoke-PnPSiteTemplate -Path "Admin.xml" -ErrorAction Stop
+Write-Host "Fim" -ForegroundColor Green
 
-}
-catch {
-    PromptError $Error[0].toString()
-}
